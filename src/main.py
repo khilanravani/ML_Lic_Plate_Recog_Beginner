74aa07951abe60bc5fbd940c2922c14a565b2171
heet <heetsankesara3@gmail.com>
2017-06-17 22:59:59 +0530

    Add Some files
    Add Some files

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
-class main:
-	def main():
+import numpy as np
+import scipy as sc
+from PIL import Image
 
-	if __name__ == '__main__':
+
+def input():
+    """
+    Work:       input function opens the image from img folder
+    Returns:    It returns variable contain image as
+                well an 2D array which contain (R, G, B)
+                value of cooardinate (x, y)
+    """
+    img = Image.open('/home/heet/Git/ML_Lic_Plate_Recog_Beginner/img/img_test1.jpg')
+    pix = img.load()
+    return img, pix
+
+
+def main():
+    img, img_array = input()
+    img.show()
+    print img_array[0, 0]
+
+
+if __name__ == '__main__':
     main()
